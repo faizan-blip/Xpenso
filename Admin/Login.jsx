4ac9e84afb962ba559f71e2ed06e559b13ee4f13
faizan-blip <faizanrock753@gmail.com>
2023-08-28 23:02:45 +0530

    done some ui changes

--- Admin/Login.jsx
+++ Admin/Login.jsx
@@ -1,24 +1,24 @@
-import { signIn, signOut, providers, useSession } from "next-auth/react"
+// import { signIn, signOut, providers, useSession } from "next-auth/react"
 
-export default function Login() {
-  const { data: session } = useSession()
+// export default function Login() {
+//   const { data: session } = useSession()
 
-  if (session) {
-    // Set the user data in sessionStorage only when the user is signed in
-    sessionStorage.setItem("user", JSON.stringify(session))
+//   if (session) {
+//     // Set the user data in sessionStorage only when the user is signed in
+//     sessionStorage.setItem("user", JSON.stringify(session))
 
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+//     return (
+//       <>
+//         Signed in as {session.user.email} <br />
+//         <button onClick={() => signOut()}>Sign out</button>
+//       </>
+//     )
+//   }
 
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       Not signed in <br />
+//       <button onClick={() => signIn('google')}>Sign in with Google</button>
+//     </>
+//   )
+// }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from "next-auth/providers/github";
-export default NextAuth ({
-    providers:[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_SECRET,
-        })
-    ]
-})
+// import NextAuth from "next-auth/next";
+// import GoogleProvider from 'next-auth/providers/google'
+// import GitHubProvider from "next-auth/providers/github";
+// export default NextAuth ({
+//     providers:[
+//         GoogleProvider({
+//             clientId:process.env.GOOGLE_ID,
+//             clientSecret:process.env.GOOGLE_SECRET,
+//         })
+//     ]
+// })
