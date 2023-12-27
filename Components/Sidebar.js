fcdab10643ea211186cfd9224a9534c6a19dab15
faizan-blip <faizanrock753@gmail.com>
2023-12-27 18:58:09 +0530

    fixed ui

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -11,7 +11,7 @@ const Sidebar = () => {
     return (  
         <>
         <Stack height='auto' display={{md:"flex" , xs:"none"}} flexDirection='column' alignItems='center' width='15%' margin='1.2em 1.2em' padding='1.5em 1.5em' borderRadius='9px' sx={{background:`url(${bg1.src})`, backgroundPosition:"center" , backgroundSize:"cover" , backgroundRepeat:"no-repeat" }} boxShadow="8px 8px 22px rgba(21, 22, 24, 0.38), -4px -2px 16px rgba(195, 200, 205, 0.08), inset -2px -2px 4px rgba(54, 54, 57, 0.16), inset 2px 2px 4px rgba(30, 30, 32, 0.18)" >
-        <Image src={logo} alt=".." width={70} height={70} style={{boxShadow: "inset 2px 2px 5px #babecc,inset -5px -5px 10px #ffffff73", width:"6em" , height:"6em" , borderRadius:"50%" , padding:"0.5em 0.5em"}} />     
+        <Image onClick={()=> router.push('/')} src={logo} alt=".." width={70} height={70} style={{boxShadow: "inset 2px 2px 5px #babecc,inset -5px -5px 10px #ffffff73", width:"6em" , height:"6em" , borderRadius:"50%" , padding:"0.5em 0.5em"}} />     
         <Button variant='contained' sx={{borderRadius:"7px" , background:"#252525 !important" , marginTop:"2em" , fontWeight:"700" , fontSize:"19px" , width:"100%"}}>Dashboard</Button>
         <Stack marginTop='2.8em' bgcolor='' boxShadow='inset 2px 2px 5px #babecc,inset -5px -5px 10px #ffffff73' width='100%' height='auto' flexDirection='column' alignItems='center' padding='1.5em 0.5em' borderRadius='12px'>
          <Box component='img' src="./detais.png" sx={{width:"200px"}}/>
