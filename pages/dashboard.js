2c79a6213e8ad29d894b8f88d684106ef4fc126b
faizan-blip <faizanrock753@gmail.com>
2024-01-14 09:10:35 +0530

    ui fixed for dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -17,7 +17,7 @@ import Image from "next/image";
 const Dashboard = () => {
   const router = useRouter();
   const {isDarkMode , setIsDarkMode} = useContext(AppContext)
-  const email = Cookies.get('email')
+  let email = Cookies.get('email')
   const { accessemail , accesstoken} = router.query;
   Cookies.set("accessemail" , accessemail)
   useEffect(() => {
@@ -41,7 +41,7 @@ const Dashboard = () => {
           <Stack flexDirection='column' alignItems='start' >
           <Image className="logo" onClick={()=> router.push(`/?accesstoken=${accesstoken}&accessemail=${accessemail}`)} src={logo} alt=".." width={50} height={50} style={{boxShadow: "inset 2px 2px 5px #babecc,inset -5px -5px 10px #ffffff73", width:"4em" , height:"4em" , borderRadius:"50%" , padding:"0.5em 0.5em"  , display:"none"}} />     
           <Typography fontSize='25px' fontWeight='700' color='#252525'>Dashboard</Typography>
-              <Typography fontSize={{sm:"15px" , xs:"12px"}}>Logged-in as {accessemail || email}</Typography>
+              <Typography fontSize={{sm:"15px" , xs:"12px"}}>Logged-in as {email == undefined ? accessemail : email}</Typography>
             </Stack>
             <Stack display={{md:"none" , xs:"block"}}>
             <Fab onClick={async()=>{
